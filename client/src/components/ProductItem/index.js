bb74c698d8908b33e89b498d4312529749ddff54
raposamillar <lisa.raposamillar@gmail.com>
2022-11-21 21:16:47 -0500

    add shopping cart persistence using IndexedDB

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductItem(item) {
 
@@ -20,7 +21,7 @@ function ProductItem(item) {
 
   const addToCart = () => {
     // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
 
     // if there was a match, call UPDATE with a new purchase quantity
     if (itemInCart) {
@@ -29,13 +30,18 @@ function ProductItem(item) {
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
-  };
+  }
 
   return (
     <div className="card px-1 py-1">
